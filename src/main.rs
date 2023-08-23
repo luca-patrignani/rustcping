459aa0bd1532cef5bfae5a95fb5260e95f26ae4d
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2023-08-23 15:10:57 +0200

    Add infos when printing probes results

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,25 @@ use std::{
 };
 
 pub mod printer;
+pub mod user_input;
 
-use printer::{Probe, ProbePrinter};
+use printer::{Probe, ProbePrinter, StdoutPrinter};
+use user_input::UserInput;
 
 fn main() {
+    let user_input = UserInput{socket: SocketAddr::from(([93, 184, 216, 34], 443))}; // example.com port 443
+    let socket = user_input.socket;
     let (sx, rx) = mpsc::channel::<Probe>();
-    // let printer = StdoutPrinter::new();
+    let printer = StdoutPrinter::new(user_input);
     thread::spawn(move || loop {
         let rcvd = rx.recv();
         if let Ok(probe) = rcvd {
-            printer::StdoutPrinter::print_probe(probe);
+            printer.print_probe(probe);
         }
     });
-    let addr = SocketAddr::from(([93, 184, 216, 34], 443)); // example.com port 443
     loop {
         let start = Instant::now();
-        let conn_res = TcpStream::connect_timeout(&addr, Duration::from_secs(1));
+        let conn_res = TcpStream::connect_timeout(&socket, Duration::from_secs(1));
         let elapsed = start.elapsed();
         let err: Option<std::io::Error> = conn_res.err();
         _ = sx.send(Probe { elapsed, err });
--- src/printer.rs
+++ src/printer.rs
@@ -1,30 +1,31 @@
 use std::time::Duration;
 
+use crate::user_input::UserInput;
+
 pub struct Probe {
     pub elapsed: Duration,
     pub err: Option<std::io::Error>,
 }
 
 pub trait ProbePrinter {
-    fn print_probe(p: Probe);
+    fn print_probe(&self, p: Probe);
 }
 
-pub struct StdoutPrinter {}
+pub struct StdoutPrinter {
+    user_input: UserInput
+}
 
 impl ProbePrinter for StdoutPrinter {
-    fn print_probe(p: Probe) {
-        println!("{} {}", p.err.is_some(), p.elapsed.as_millis());
-    }
-}
-
-impl Default for StdoutPrinter {
-    fn default() -> Self {
-        Self::new()
+    fn print_probe(&self, p: Probe) {
+        let elapsed = p.elapsed.as_millis();
+        let ip = self.user_input.socket.ip();
+        let port = self.user_input.socket.port();
+        println!("Reply from 74.6.231.21 ({ip}) on port {port} TCP_conn=1 time={elapsed} ms")
     }
 }
 
 impl StdoutPrinter {
-    pub fn new() -> StdoutPrinter {
-        StdoutPrinter {}
+    pub fn new(user_input: UserInput) -> StdoutPrinter {
+        StdoutPrinter {user_input}
     }
 }
