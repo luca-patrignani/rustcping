6c7f0f362ccafc8e315662b0e3a957b9c25bbd62
Luca Patrignani <luca0patrignani@gmail.com>
2023-08-23 10:05:56 +0200

    Loop forever and print the elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{
+    net::{SocketAddr, TcpStream},
+    thread,
+    time::{Duration, Instant},
+};
 fn main() {
-    println!("Hello, world!");
+    let addr = SocketAddr::from(([93, 184, 216, 34], 443)); // example.com port 443
+    loop {
+        let start = Instant::now();
+        let conn_res = TcpStream::connect_timeout(&addr, Duration::from_secs(1));
+        let elapsed = start.elapsed();
+        let err: Option<std::io::Error> = conn_res.err();
+        println!("{}, {}", err.is_some(), elapsed.as_millis());
+        thread::sleep(Duration::from_secs(1) - elapsed)
+    }
 }
