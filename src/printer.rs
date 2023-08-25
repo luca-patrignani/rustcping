f87fbb0a4201ef9fae23b2835801be0e369cd5f9
Luca Patrignani <luca0patrignani@gmail.com>
2023-08-25 11:09:38 +0200

    Write final stats

--- src/printer.rs
+++ src/printer.rs
@@ -8,12 +8,38 @@ pub fn print_probe(info: &Info, probe: &Probe) {
     }
 }
 
+pub fn print_final_stats(info: &Info) {
+    let succ_counter = info.succ_probes_counter;
+    let fail_counter = info.fail_probes_counter;
+    let total_probes = succ_counter + fail_counter;
+    println!(
+"
+--- example.com (93.184.216.34) TCPing statistics ---
+{total_probes} probes transmitted on port 443 | {succ_counter} received, 62.50% packet loss
+successful probes:   {succ_counter}
+unsuccessful probes: {fail_counter}
+last successful probe:   2023-08-25 09:42:47
+last unsuccessful probe: 2023-08-25 09:42:46
+total uptime:   3 seconds
+total downtime: 5 seconds
+longest consecutive uptime:   2 seconds from 2023-08-25 09:42:40 to 2023-08-25 09:42:42
+longest consecutive downtime: 5 seconds from 2023-08-25 09:42:42 to 2023-08-25 09:42:47
+retried to resolve hostname 0 times
+rtt min/avg/max: 131.306/132.387/133.411 ms
+--------------------------------------
+TCPing started at: 2023-08-25 09:42:40
+TCPing ended at:   2023-08-25 09:42:48
+duration (HH:MM:SS): 00:00:08
+"
+        )
+}
+
 fn print_probe_success(info: &Info, probe: &Probe) {
     let url = &info.user_input.url;
     let ip_addr = info.ip_addr;
     let port = info.user_input.port;
     let elapsed = probe.elapsed.as_millis();
-    let counter = info.succ_probes_counter;
+    let counter = info.succ_probes_streak;
     println!("Reply from {url} ({ip_addr}) on port {port} TCP_conn={counter} time={elapsed} ms")
 }
 
@@ -21,6 +47,6 @@ fn print_probe_failure(info: &Info) {
     let url = &info.user_input.url;
     let ip_addr = info.ip_addr;
     let port = info.user_input.port;
-    let counter = info.fail_probes_counter;
+    let counter = info.fail_probes_streak;
     println!("No reply from {url} ({ip_addr}) on port {port} TCP_conn={counter}")
 }
--- src/tracker.rs
+++ src/tracker.rs
@@ -9,19 +9,32 @@ pub struct Probe {
 
 pub struct Info {
     pub user_input: UserInput,
+    pub succ_probes_streak: u128,
+    pub fail_probes_streak: u128,
     pub succ_probes_counter: u128,
     pub fail_probes_counter: u128,
     pub ip_addr: IpAddr,
 }
 
 impl Info {
+    pub fn new(user_input: UserInput, ip_addr: IpAddr) -> Info {
+        Info{user_input, ip_addr, 
+            succ_probes_streak: 0, 
+            fail_probes_streak: 0,
+            succ_probes_counter: 0,
+            fail_probes_counter: 0,
+        }
+    }
+
     pub fn track(&mut self, probe: &Probe) {
         if probe.err.is_none() {
+            self.succ_probes_streak += 1;
+            self.fail_probes_streak = 0;
             self.succ_probes_counter += 1;
-            self.fail_probes_counter = 0;
         } else {
-            self.succ_probes_counter = 0;
-            self.fail_probes_counter += 1;
+            self.succ_probes_streak = 0;
+            self.fail_probes_streak += 1;
+            self.succ_probes_counter += 1;
         }
     }
 
