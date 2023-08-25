d1e5726e950c156155cd2b72f67c083d3b28b830
Luca Patrignani <luca0patrignani@gmail.com>
2023-08-25 11:26:12 +0200

    Show url, ip_addr, port and packet loss ratio in final stats

--- src/printer.rs
+++ src/printer.rs
@@ -9,13 +9,18 @@ pub fn print_probe(info: &Info, probe: &Probe) {
 }
 
 pub fn print_final_stats(info: &Info) {
+    let url = &info.user_input.url;
+    let ip_addr = &info.ip_addr;
+    let port = info.user_input.port;
     let succ_counter = info.succ_probes_counter;
     let fail_counter = info.fail_probes_counter;
     let total_probes = succ_counter + fail_counter;
+    // awful workaround for showing two decimal figures.
+    let packet_loss_perc = (fail_counter as f64 / total_probes as f64 * 10000.0).trunc() / 100.0;
     println!(
 "
---- example.com (93.184.216.34) TCPing statistics ---
-{total_probes} probes transmitted on port 443 | {succ_counter} received, 62.50% packet loss
+--- {url} ({ip_addr}) TCPing statistics ---
+{total_probes} probes transmitted on port {port} | {succ_counter} received, {packet_loss_perc}% packet loss
 successful probes:   {succ_counter}
 unsuccessful probes: {fail_counter}
 last successful probe:   2023-08-25 09:42:47
