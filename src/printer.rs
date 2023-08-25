339ad4d7c88f63cd15dd7e1a917bce7f6840f218
Luca Patrignani <luca0patrignani@gmail.com>
2023-08-25 09:24:41 +0200

    Print probe failures

--- src/printer.rs
+++ src/printer.rs
@@ -1,10 +1,26 @@
 use crate::tracker::{Info, Probe};
 
 pub fn print_probe(info: &Info, probe: &Probe) {
+    if probe.err.is_none() {
+        print_probe_success(info, probe)
+    } else {
+        print_probe_failure(info)
+    }
+}
+
+fn print_probe_success(info: &Info, probe: &Probe) {
     let url = &info.user_input.url;
     let ip_addr = info.ip_addr;
     let port = info.user_input.port;
     let elapsed = probe.elapsed.as_millis();
-    let counter = info.counter;
+    let counter = info.succ_probes_counter;
     println!("Reply from {url} ({ip_addr}) on port {port} TCP_conn={counter} time={elapsed} ms")
+}
+
+fn print_probe_failure(info: &Info) {
+    let url = &info.user_input.url;
+    let ip_addr = info.ip_addr;
+    let port = info.user_input.port;
+    let counter = info.fail_probes_counter;
+    println!("No reply from {url} ({ip_addr}) on port {port} TCP_conn={counter}")
 }
--- src/tracker.rs
+++ src/tracker.rs
@@ -9,16 +9,19 @@ pub struct Probe {
 
 pub struct Info {
     pub user_input: UserInput,
-    pub counter: u128,
+    pub succ_probes_counter: u128,
+    pub fail_probes_counter: u128,
     pub ip_addr: IpAddr,
 }
 
 impl Info {
     pub fn track(&mut self, probe: &Probe) {
         if probe.err.is_none() {
-            self.counter += 1;
+            self.succ_probes_counter += 1;
+            self.fail_probes_counter = 0;
         } else {
-            self.counter = 0;
+            self.succ_probes_counter = 0;
+            self.fail_probes_counter += 1;
         }
     }
 
