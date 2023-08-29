67782114dcabdfc6037454ab126cd978058f49dc
Luca Patrignani <luca0patrignani@gmail.com>
2023-08-29 10:32:32 +0200

    Remove the awful workaround for showing probes ratio with two decimal figures

--- src/printer.rs
+++ src/printer.rs
@@ -17,8 +17,7 @@ pub fn print_final_stats(info: &Info) {
     let succ_counter = info.succ_probes_counter;
     let fail_counter = info.fail_probes_counter;
     let total_probes = succ_counter + fail_counter;
-    // awful workaround for showing two decimal figures.
-    let packet_loss_perc = (fail_counter as f64 / total_probes as f64 * 10000.0).trunc() / 100.0;
+    let packet_loss_perc = format!("{:.2}", fail_counter as f64 / total_probes as f64 * 100.0);
     let last_succ_probe = to_string(info.last_succ_probe, "Never succeded".to_owned());
     let last_fail_probe = to_string(info.last_fail_probe, "Never failed".to_owned());
     let total_uptime = info.total_uptime.num_seconds();
